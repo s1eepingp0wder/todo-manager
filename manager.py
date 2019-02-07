633e5e82e6845df16fc4c7b91057cb99e732b6a1
Danny Liehr <dannyisyelling@gmail.com>
2019-02-07 10:32:53 -0500

    The thing actually runs at least.

--- manager.py
+++ manager.py
@@ -6,6 +6,17 @@ class Manager():
         self.task_description = task_description
 
     def write_task(self):
-        f = open("todos.txt", "a")
-        f.write()
-        print("Check the file.")
+        f = open("./todos.txt", 'a+')
+        print(f.readlines())
+
+        # file = eval(f)
+        #     # new_dict= {"Task Name" : task_name, "Task Description": task_description}
+
+        # We need to pull any current dicts from this.
+        #  try:
+        #     file = eval(f)
+        #     # new_dict= {"Task Name" : task_name, "Task Description": task_description}
+        #     print(len(file))
+        # # print("Check the file.")
+        # except:
+        #     print("Can't do anything yet.")
